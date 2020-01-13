1f430edbcd8f567fb0c60737ee89d7e38524b5ed
Jay Bimal Mehta <jaybimalmehta@Jays-MacBook-Pro-2.local>
2020-01-13 18:06:38 +0530

    intial commit

---
+++ src/main/java/com/example/employeeDataTransfer/EmployeeDataTransferApplication.java
@@ -0,0 +1,13 @@
+package com.example.employeeDataTransfer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeDataTransferApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeDataTransferApplication.class, args);
+	}
+
+}
